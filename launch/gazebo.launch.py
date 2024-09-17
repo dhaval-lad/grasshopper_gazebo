c69635f88dc32ee36b06d0047f52ff52f9d8e8d9
dhaval_lad <dhavalkumarlad76@gmail.com>
2024-09-17 22:44:42 +0200

    Created pkg for different worlds for grasshopper to drive around , also added 2 actor models for dynamic objects.

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,126 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+    use_sim_time = True
+
+    world_path = PathJoinSubstitution(
+        [FindPackageShare("grasshopper_gazebo"), "worlds", "smalltown_final.world"]
+    )
+
+    description_launch_path = PathJoinSubstitution(
+        [FindPackageShare('grasshopper_description'), 'launch', 'description.launch.py']
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('grasshopper_gazebo'), 'rviz', 'gazebo.rviz']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='world', 
+            default_value=world_path,
+            description='Gazebo world'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='true',
+            description='Run rviz'
+        ),
+
+        DeclareLaunchArgument(
+            name='spawn_x', 
+            default_value='-19.5',
+            description='Robot spawn position in X axis'
+        ),
+
+        DeclareLaunchArgument(
+            name='spawn_y', 
+            default_value='52.5',
+            description='Robot spawn position in Y axis'
+        ),
+
+        DeclareLaunchArgument(
+            name='spawn_z', 
+            default_value='0.1',
+            description='Robot spawn position in Z axis'
+        ),
+            
+        DeclareLaunchArgument(
+            name='spawn_yaw', 
+            default_value='0.0',
+            description='Robot spawn heading'
+        ),
+
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', LaunchConfiguration('world')],
+            output='screen'
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='urdf_spawner',
+            output='screen',
+            arguments=[
+                '-topic', 'robot_description', 
+                '-entity', 'linorobot2', 
+                '-x', LaunchConfiguration('spawn_x'),
+                '-y', LaunchConfiguration('spawn_y'),
+                '-z', LaunchConfiguration('spawn_z'),
+                '-Y', LaunchConfiguration('spawn_yaw'),
+            ]
+        ),
+
+        Node(
+            package='linorobot2_gazebo',
+            executable='command_timeout.py',
+            name='command_timeout'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            # arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{'use_sim_time': str(use_sim_time)}]
+        ),
+
+        Node(
+            package='tf2_ros', 
+            executable='static_transform_publisher', 
+            name='static_transform_publisher_1st',
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
+        ), 
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(description_launch_path),
+            launch_arguments={
+                'use_sim_time': str(use_sim_time),
+                'publish_joints': 'false',
+            }.items()
+        )
+    ])
